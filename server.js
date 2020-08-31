a619344c4f26a4a5fd02e5db89d46747f92570d6
Jari Haavisto <jari.haavisto@gmail.com>
2020-08-31 13:19:15 +0300

    testing 4

--- server.js
+++ server.js
@@ -5,24 +5,30 @@ const app = express()
 app.use(express.json())
 
 const connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    port: process.env.RDS_PORT
 });
 
 app.get('/', (request, response) => {
-    var message
+    var message = 'Something strange in the neighbourhood?'
 
-    connection.connect(function(err) {
+    connection.connect(function (err) {
         if (err) {
-          message = 'Database connection failed: ' + err.stack
-          return;
+            message = 'Database connection failed: ' + err.stack
+            console.log(message)
+            return response.send(message)
         }
-      
+
         message = 'Connected to database.'
-      });
+        console.log(message)
+        return response.send(message)
+    });
+
+    console.log('should not get this far?')
     response.send(message)
+
 })
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening...`))
