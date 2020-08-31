cc6c2c238d323a6873b2f1c5e98228116c8d964e
Jari Haavisto <jari.haavisto@gmail.com>
2020-08-31 13:43:35 +0300

    fix database name

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT,
-    database: process.env.RDS_DATABASE,
+    database: process.env.RDS_DB_NAME,
     user: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD
 }).promise();
