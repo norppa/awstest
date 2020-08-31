b2e40fd5e298996f194542d3b7b90add6d6bb4b6
Jari Haavisto <jari.haavisto@gmail.com>
2020-08-31 13:04:06 +0300

    testing 2

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 
 app.use(express.json())
+
+const connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
 
 app.get('/', (request, response) => {
     response.send('Running...')
