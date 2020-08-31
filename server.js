c571a00f67e61c668aa7c9cdf28b8a3bc7300fd9
Jari Haavisto <jari.haavisto@gmail.com>
2020-08-31 13:00:27 +0300

    testing

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
 const express = require('express')
-const mysql = require('mysql')
 const app = express()
 
 app.use(express.json())
 
-var connection = mysql.createConnection({
-    host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
-    password : process.env.RDS_PASSWORD,
-    port     : process.env.RDS_PORT
-  });
-
-
-
 app.get('/', (request, response) => {
-    let response
-
-    connection.connect(function(err) {
-        if (err) {
-          response = 'Database connection failed: ' + err.stack
-          return
-        }
-      
-        response = 'Connected to database.'
-      });
-    response.send(response)
+    response.send('Running...')
 })
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening...`))
